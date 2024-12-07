a0d9dc73bfa88fb93246ef5d7dfab349683d6d06
pablogalcas <pablogalcas@uma.es>
2024-12-07 10:56:45 +0100

    Todos los botones funcionando
    Falta la diferenciacion entre los usuarios y los roles

--- backend/routes/piezas.py
+++ backend/routes/piezas.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from backend.models.tPiezas import TPiezas
+from backend.app import db
 
 piezas_bp = Blueprint('piezas', __name__)
 
@@ -20,3 +21,58 @@ def obtener_piezas_por_tipo(tipo_id):
 
     except Exception as e:
         return jsonify({"message": f"Error al obtener piezas: {str(e)}"}), 500
+    
+@piezas_bp.route('/piezas/insertar', methods=['POST'])
+def insertar_pieza():
+    try:
+        data = request.get_json()
+        nombre = data.get("nombre")
+        fabricante = data.get("fabricante")
+        id_tipo = data.get("id_tipo")
+
+        if not nombre or not fabricante or not id_tipo:
+            return jsonify({"message": "Faltan datos"}), 400
+        
+        nueva_pieza = TPiezas(nombre = nombre, fabricante = fabricante, id_tipo = id_tipo)
+
+        db.session.add(nueva_pieza)
+        db.session.commit()
+
+        return jsonify({"message": "Pieza añadida correctamente"}), 201
+    except Exception as e:
+        return jsonify({"message": f"Error al agregar la pieza: {str(e)}"}), 500
+    
+@piezas_bp.route('/piezas/actualizar/<int:id_pieza>', methods=['PUT'])
+def actualizar_pieza(id_pieza):
+    try:
+        data = request.get_json()
+        nombre = data.get("nombre")
+        fabricante = data.get("fabricante")
+        
+        if not nombre or not fabricante:
+            return jsonify({"message": "Faltan datos"}), 400
+        
+        pieza = TPiezas.query.get(id_pieza)
+
+        if not pieza:
+            return jsonify({"message": "Pieza no encontrada"}), 400
+        
+        pieza.nombre = nombre
+        pieza.fabricante = fabricante
+        db.session.commit()
+        return jsonify({"message": "Pieza actualizada correctamente"}), 201
+    except Exception as e:
+        return jsonify({"message": f"Error al agregar la pieza: {str(e)}"}), 500
+
+@piezas_bp.route('/piezas/borrar/<int:id_pieza>', methods=['DELETE'])
+def borrar_pieza(id_pieza):
+    try:
+        pieza = TPiezas.query.get(id_pieza)
+
+        if not pieza:
+            return jsonify({"message": "Pieza no encontrada"}), 400
+        db.session.delete(pieza)
+        db.session.commit()
+        return jsonify({"message": "Pieza borrada correctamente"}), 201
+    except Exception as e:
+        return jsonify({"message": f"Error al agregar la pieza: {str(e)}"}), 500
--- frontend/ventanas/piezas_taller.py
+++ frontend/ventanas/piezas_taller.py
@@ -26,7 +26,7 @@ class VentanaPiezasTaller:
         #self.label_nombre.grid(column=0, row=0, padx=10, pady=10, sticky="e")
         self.label_nombre.place(x=40, y=360)
 
-        # Label Fabriacante
+        # Label Fabricante
         self.label_fabricante = tk.Label(self.master, text="Fabricante")
         #self.label_fabricante.grid(column=0, row=0, padx=10, pady=10, sticky="e")
         self.label_fabricante.place(x=40, y=330)        
@@ -35,7 +35,7 @@ class VentanaPiezasTaller:
         self.listbox_tipos = tk.Listbox(self.master, width=30, height=7)
         #self.listbox_tipos.grid(column=1, row=0, padx=10, pady=10, sticky="w")
         self.listbox_tipos.place(x=200, y=40, height= 40)
-        self.listbox_tipos.bind("<<ListboxSelect>>", self.mostrar_piezas)
+        self.listbox_tipos.bind("<<ListboxSelect>>", self.seleccion_listbox)
 
         # Crear la Scrollbar: EN PROGRESO
         
@@ -50,7 +50,7 @@ class VentanaPiezasTaller:
         self.treeview_piezas = ttk.Treeview(self.master, columns=("id", "nombre", "fabricante", "id_tipo"), show="headings", height=9)
         #self.treeview_piezas.grid(row=1, columnspan=2, padx=40, pady=40, sticky="we")
         self.treeview_piezas.place(x=50, y=115, width= 500)
-        self.treeview_piezas.bind("<<TreeviewSelect>>", self.mostrar_pieza_seleccionada)
+        self.treeview_piezas.bind("<<TreeviewSelect>>", self.seleccion_treeview)
 
         # Columnas treeview
         self.treeview_piezas.heading("id", text="ID")
@@ -62,29 +62,32 @@ class VentanaPiezasTaller:
         self.treeview_piezas.column("fabricante", width=100, anchor="center")
         self.treeview_piezas.column("id_tipo", width=50, anchor="center")
 
-        # Caja de texto correspondiente al nombre de la pieza
-        self.nombre_pieza = tk.Entry(self.master)
-        #self.nombre_pieza.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-        self.nombre_pieza.place(x=110, y=330, width= 400)
+        self.nombre_var = tk.StringVar()
+        self.fabricante_var = tk.StringVar()
 
         # Caja de texto correspondiente al nombre del fabricante
-        self.fabricante_pieza = tk.Entry(self.master)
+        self.entry_fabricante = tk.Entry(self.master, textvariable=self.nombre_var)
+        #self.nombre_pieza.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.entry_fabricante.place(x=110, y=330, width= 400)
+
+        # Caja de texto correspondiente al nombre de la pieza
+        self.entry_nombre = tk.Entry(self.master, textvariable=self.fabricante_var)
         #self.fabricante_pieza.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-        self.fabricante_pieza.place(x=110, y=360, width= 400)
+        self.entry_nombre.place(x=110, y=360, width= 400)
 
         # Botón Insertar
-        self.insert_button = tk.Button(self.master, text="Insertar", command= self.addPieza)
+        self.insert_button = tk.Button(self.master, text="Insertar", command= self.insertar_pieza)
         #self.insert_button.grid(row=3, column=0, padx=40, pady=40, sticky="ew")
         self.insert_button.place(x=90, y=410, width= 50)
         
         # Botón Borrar
-        self.delete_button = tk.Button(self.master, text="Borrar")
+        self.delete_button = tk.Button(self.master, text="Borrar", command = self.borrar_pieza)
         #self.delete_button.grid(row=3, column=0, padx=40, pady=40, sticky="ew")
         self.delete_button.place(x= 200, y=410, width= 50)
 
 
         # Botón Actualizar
-        self.actualizar_button = tk.Button(self.master, text="Actualizar")
+        self.actualizar_button = tk.Button(self.master, text="Actualizar", command = self.actualizar_pieza)
         #self.actualizar_button.grid(row=3, column=0, padx=40, pady=40, sticky="ew")
         self.actualizar_button.place(x= 310, y=410, width= 70)
         
@@ -117,7 +120,10 @@ class VentanaPiezasTaller:
         except Exception as e:
             tk.messagebox.showerror("Error", f"No se pudo conectar al servidor: {e}")
 
-    def mostrar_piezas(self, event):
+    def seleccion_listbox(self, event):
+        self.mostrar_piezas()
+
+    def mostrar_piezas(self):
         # Limpiar el Treeview
         for item in self.treeview_piezas.get_children():
             self.treeview_piezas.delete(item)
@@ -145,39 +151,95 @@ class VentanaPiezasTaller:
         except Exception as e:
             messagebox.showerror("Error", f"No se pudo conectar al servidor: {e}")
 
-    def mostrar_pieza_seleccionada(self, event):
+    def seleccion_treeview(self, event):
+        self.mostrar_pieza_seleccionada()
+
+    def mostrar_pieza_seleccionada(self):
         # Si hay un elemento seleccionado, mostramos su información
         item_id = self.treeview_piezas.focus()
         if item_id != "":
-          valores = self.treeview_piezas.item(item_id, "values")          
-          self.nombre_pieza.delete(0, 'end')
-          self.nombre_pieza.insert(0, valores[2])
-          self.fabricante_pieza.delete(0, 'end')
-          self.fabricante_pieza.insert(0, valores[1])
+          valores = self.treeview_piezas.item(item_id, "values")
+          self.nombre_var.set(valores[2])
+          self.fabricante_var.set(valores[1])
 
     def limpiar_seleccion(self):        
-        self.treeview_piezas.selection_remove(self.treeview_piezas.selection())       
-        self.nombre_pieza.delete(0, 'end')
-        self.fabricante_pieza.delete(0, 'end')
-    
+        # Limpiar selección del Treeview
+        self.treeview_piezas.selection_remove(self.treeview_piezas.selection())
+        self.treeview_piezas.focus("")
+        # Limpiar los Textboxes
+        self.nombre_var.set("")
+        self.fabricante_var.set("")
+
     #Función para añadir pieza a la base de datos
+    def insertar_pieza(self):
+        nombre = self.entry_nombre.get()
+        fabricante = self.entry_fabricante.get()
 
-    def addPieza(self):
-     
-     nombre = self.nombre_pieza.get()
-     fabricante = self.fabricante_pieza.get()
+        # Verificar si se seleccionó un tipo de pieza
+        seleccion = self.listbox_tipos.curselection()
+        if not seleccion:
+            messagebox.showerror("Error", "Debe seleccionar un tipo de pieza.")
+            return
 
-     url = "http://127.0.0.1:5000/piezas"
-     data = {"nombre": nombre, "fabricante": fabricante}
-     try:
-        response = requests.post(url, json=data)
-        if response.status_code == 200:
-            self.mostrar_piezas()
-        else:
-            messagebox.showerror("Error", response.json().get("message", "Error desconocido"))
-     except requests.exceptions.RequestException as e:
-            messagebox.showerror("Error", f"No se puedo conectar al servidor: {e}")
+        url = "http://127.0.0.1:5000/piezas/insertar"
+        data = {"nombre": nombre, "fabricante": fabricante, "id_tipo": self.tipos_dict.get(self.listbox_tipos.get(seleccion[0]))}
+        try:
+            response = requests.post(url, json=data)
+            if response.status_code == 201:
+                self.limpiar_seleccion()
+                self.mostrar_piezas()
+            else:
+                messagebox.showerror("Error", response.json().get("message", "Error desconocido"))
+        except requests.exceptions.RequestException as e:
+                messagebox.showerror("Error", f"No se puedo conectar al servidor: {e}")
 
+    def actualizar_pieza(self):
+        seleccion = self.listbox_tipos.curselection()
+        if not seleccion:
+            messagebox.showerror("Error", "Debe seleccionar un tipo de pieza.")
+            return
+        
+        item_id = self.treeview_piezas.focus()
+        if item_id != "":
+            valores = self.treeview_piezas.item(item_id, "values")
+            id = valores[0]
+            nombre = self.entry_nombre.get()
+            fabricante = self.entry_fabricante.get()
+
+            url = f"http://127.0.0.1:5000/piezas/actualizar/{id}"
+            data = {"nombre": nombre, "fabricante": fabricante}
+            try:
+                response = requests.put(url, json=data)
+                if response.status_code == 201:
+                    self.limpiar_seleccion()
+                    self.mostrar_piezas()
+                else:
+                    messagebox.showerror("Error", response.json().get("message", "Error desconocido"))
+            except requests.exceptions.RequestException as e:
+                    messagebox.showerror("Error", f"No se puedo conectar al servidor: {e}")
+
+    def borrar_pieza(self):
+        seleccion = self.listbox_tipos.curselection()
+        if not seleccion:
+            messagebox.showerror("Error", "Debe seleccionar un tipo de pieza.")
+            return
+        
+        item_id = self.treeview_piezas.focus()
+        if item_id != "":
+            valores = self.treeview_piezas.item(item_id, "values")
+            id = valores[0]
+
+            url = f"http://127.0.0.1:5000/piezas/borrar/{id}"
+            try:
+                response = requests.delete(url)
+                if response.status_code == 201:
+                    self.limpiar_seleccion()
+                    self.mostrar_piezas()
+                else:
+                    messagebox.showerror("Error", response.json().get("message", "Error desconocido"))
+            except requests.exceptions.RequestException as e:
+                    messagebox.showerror("Error", f"No se puedo conectar al servidor: {e}")
+            
 
 
 
